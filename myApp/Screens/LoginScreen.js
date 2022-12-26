990947c041704510b957d6846d7f8c21f536a71a
Zhenya Pastukhov <amamahahatala@hotmail.com>
2022-12-26 23:21:54 +0200

    A

---
+++ myApp/Screens/LoginScreen.js
@@ -0,0 +1,131 @@
+import { useState } from "react";
+
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Keyboard,
+} from "react-native";
+
+const initialState = {
+  email: "",
+  password: "",
+};
+export const LoginScreen = ({
+  isShowKeyboard,
+  setIsShowKeyboard,
+  dimensions,
+}) => {
+  const [state, setState] = useState(initialState);
+
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    console.log(state);
+    setState(initialState);
+  };
+
+  return (
+    <View style={styles.formBox}>
+      <Text style={styles.titleForm}>Sign In</Text>
+      <View
+        style={{
+          ...styles.form,
+          marginBottom: isShowKeyboard ? -90 : 144,
+          marginHorizontal: dimensions < 400 ? 16 : 50,
+        }}
+      >
+        <TextInput
+          placeholder="Email"
+          value={state.email}
+          style={styles.input}
+          onFocus={() => setIsShowKeyboard(true)}
+          onBlur={() => {
+            setIsShowKeyboard(false);
+          }}
+          onChangeText={(value) =>
+            setState((prevState) => ({ ...prevState, email: value }))
+          }
+        />
+        <TextInput
+          placeholder="Password"
+          value={state.password}
+          style={{ ...styles.input, marginBottom: 43 }}
+          secureTextEntry={true}
+          onFocus={() => setIsShowKeyboard(true)}
+          onBlur={() => {
+            setIsShowKeyboard(false);
+          }}
+          onChangeText={(value) =>
+            setState((prevState) => ({ ...prevState, password: value }))
+          }
+        />
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={styles.btn}
+          onPress={keyboardHide}
+        >
+          <Text style={styles.btnTitle}>Sign in</Text>
+        </TouchableOpacity>
+        <Text style={styles.bottomText}>No account? Sign up.</Text>
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  formBox: {
+    backgroundColor: "#fff",
+    paddingTop: 32,
+
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    position: "relative",
+  },
+
+  titleForm: {
+    textAlign: "center",
+    marginBottom: 32,
+    fontSize: 30,
+    lineHeight: 35.16,
+    fontFamily: "Roboto-Med",
+  },
+  form: { marginHorizontal: 16 },
+  input: {
+    padding: 16,
+    fontSize: 16,
+    lineHeight: 19,
+    fontFamily: "Roboto-Reg",
+    backgroundColor: "#F6F6F6",
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+
+  btn: {
+    paddingTop: 16,
+    paddingBottom: 16,
+
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    marginBottom: 16,
+  },
+  btnTitle: {
+    fontFamily: "Roboto-Reg",
+    fontSize: 16,
+    lineHeight: 19,
+    marginBottom: 16,
+    color: "#fff",
+    marginBottom: 0,
+    textAlign: "center",
+  },
+  bottomText: {
+    textAlign: "center",
+    fontSize: 16,
+    lineHeight: 19,
+    fontFamily: "Roboto-Reg",
+    color: "#1B4371",
+  },
+});
