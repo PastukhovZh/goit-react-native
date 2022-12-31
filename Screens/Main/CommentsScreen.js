7a29ab6ec38e71b1e9d5b0ff6a0fd69630633d66
Zhenya Pastukhov <amamahahatala@hotmail.com>
2022-12-31 23:52:33 +0200

    Revert "y"
    This reverts commit 758ca22a9811fe8d2372a52cbf8b1ae820b9d79b.

--- Screens/Main/CommentsScreen.js
+++
@@ -1,9 +0,0 @@
-import { TextInput } from "react-native-gesture-handler"
-
-const CommentsScreen = ()=>{
-    return(
-        <TextInput style={{paddingTop:100}}>CommentsScreen</TextInput>
-    )
-}
-
-export default CommentsScreen;
--- Screens/Main/MapScreen.js
+++
@@ -1,7 +0,0 @@
-import { TextInput } from "react-native-gesture-handler";
-
-const MapScreen = () => {
-  return <TextInput>MapScreen</TextInput>;
-};
-
-export default MapScreen;
